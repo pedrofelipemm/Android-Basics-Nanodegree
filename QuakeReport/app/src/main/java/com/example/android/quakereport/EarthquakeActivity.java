38f09654cb0af92672d6235eb5e1c3cabcc62b5c
Pedro Felipe Marques Moreira <pedrofelipemm@gmail.com>
2017-02-26 11:13:23 -0300

    QuakeReport - initial commit

---
+++ QuakeReport/app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -0,0 +1,34 @@
+package com.example.android.quakereport;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class EarthquakeActivity extends AppCompatActivity {
+
+    public static final String LOG = EarthquakeActivity.class.getName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.earthquake_activity);
+
+        ArrayList<String> earthquakes = new ArrayList<>();
+        earthquakes.add("San Francisco");
+        earthquakes.add("London");
+        earthquakes.add("Tokyo");
+        earthquakes.add("Mexico City");
+        earthquakes.add("Moscow");
+        earthquakes.add("Rio de Janeiro");
+        earthquakes.add("Paris");
+
+        ListView earthquakeListView = (ListView) findViewById(R.id.list);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, earthquakes);
+
+        earthquakeListView.setAdapter(adapter);
+    }
+}
